ddb85988de9df1681cc73069688374d48af8aa02
Aleksei Nikolashkin <nagvv97@mail.ru>
2024-05-27 02:07:59 +0300

    add clearing niching mechanism

--- insectae/decorators.py
+++ insectae/decorators.py
@@ -1,12 +1,13 @@
 from copy import deepcopy
 from types import MethodType
-from typing import Any, Callable, List, Optional, Union
+from typing import Any, Callable, List, Optional, Tuple, Union
 
 import numpy as np
 from numpy.typing import NDArray
 
 from .alg_base import Algorithm
 from .common import l2metrics
+from .goals import Goal
 from .timer import Timer
 from .typing import Environment, Individual
 
@@ -249,3 +250,140 @@ class AddFitnessSharing:
         else:
             f_value *= ind[self._niche_count_key]
         ind[keyf] = f_value
+
+
+class AddClearing:
+    """
+    The class to inject the clearing niching mechanism into the given algorithm.
+    It implements clearing procedure that is described in the paper [1]_.
+
+    This class is applicable to any algorithm which uses evaluate pattern from
+    its executor. Any other fitness evaluations outside that are not tracked.
+
+    Parameters
+    ----------
+    sigma : float
+        The clearing radius.
+    capacity : int
+        The capacity of each niche.
+    dist_func : Callable[[NDArray, NDArray], float], default: l2metrics
+        The distance function used to compute the distance between individuals.
+    niched_solution_value_key : str, default: "clearing_f"
+        The key under which the niched solution value is stored after the
+        algorithm is executed.
+
+    References
+    -----
+    .. [1] Pétrowski A. A clearing procedure as a niching method for genetic
+       algorithms //Proceedings of IEEE international conference on evolutionary
+       computation. – IEEE, 1996. – С. 798-803.
+    """
+
+    def __init__(
+        self,
+        sigma: float,
+        capacity: int,
+        dist_func: Callable[[NDArray, NDArray], float] = l2metrics,
+        niched_solution_value_key: str = "clearing_f",
+    ) -> None:
+        self._sigma = sigma
+        self._capacity = capacity
+        self._dist_func = dist_func
+        self._orig_f_key = "_clearing_orig_f"
+        self._is_niche_winner_key = "_clearing_is_niche_winner"
+        self._niched_solution_value_key = niched_solution_value_key
+        self._goal = "min"
+
+    def __call__(self, algorithm: Algorithm) -> None:
+        if "AddClearing" in algorithm.decorators:
+            return
+
+        self._goal = algorithm.goal.getDir()
+
+        def _evaluate(
+            self_executor,
+            population: List[Individual],
+            keyx: str,
+            keyf: str,
+            env: Environment,
+            **kwargs
+        ) -> None:
+            timer = env.get("timer")
+            self_executor._clearing_orig_evaluate(
+                population=population,
+                keyx=keyx,
+                keyf=self._orig_f_key,
+                env=env,
+                **kwargs
+            )
+            self_executor.signals(
+                population=population,
+                metrics=self._dist_func,
+                shape=self._add_context_to_distances,
+                reduce=self._compute_niche,
+                keyx=keyx,
+                keys=self._is_niche_winner_key,
+                env=env,
+                timingLabel="clearing",
+                timer=timer,
+            )
+            self_executor.foreach(
+                population=population,
+                op=self._apply_niching,
+                fnkwargs={"keyf": keyf, "goal": algorithm.goal},
+                timingLabel="clearing",
+                timer=timer,
+            )
+
+        def _start(_: List[Individual], env: Environment) -> None:
+            executor = algorithm.executor
+            executor._clearing_orig_evaluate = executor.evaluate
+            executor.evaluate = MethodType(_evaluate, executor)
+
+        def _finish(population: List[Individual], env: Environment) -> None:
+            keyf = env["solutionValueLabel"]
+
+            def _save_niched_solution_value(ind: Individual):
+                ind[self._niched_solution_value_key] = ind[keyf]
+
+            executor = algorithm.executor
+            executor.evaluate = executor._clearing_orig_evaluate
+            del executor._clearing_orig_evaluate
+            executor.foreach(
+                population=population, op=_save_niched_solution_value, fnkwargs={}
+            )
+            # run evaluate again without niching to get the actual solution
+            # values; we can't simply replace them with existing orig values
+            # since after evaluate algorithm can tamper with them
+            executor.evaluate(
+                population=population,
+                keyx=env["solutionLabel"],
+                keyf=keyf,
+                env=env,
+                timingLabel="clearing",
+                timer=env.get("timer"),
+            )
+
+        algorithm.addProcedure("start", _start)
+        algorithm.addProcedure("finish", _finish)
+        algorithm.decorators.append("AddClearing")
+
+    def _add_context_to_distances(
+        self, d, inds: Tuple[Individual, Individual], env: Environment
+    ):
+        return d, inds[1][self._orig_f_key], inds[0] is inds[1]
+
+    def _compute_niche(self, d_f_s_tuples: NDArray) -> int:
+        niche = d_f_s_tuples[d_f_s_tuples[:, 0] <= self._sigma]
+        cap = min(self._capacity, len(niche))
+        if self._goal == "min":
+            niche = niche[niche[:, 1].argpartition(kth=cap - 1)[:cap]]
+        else:
+            niche = niche[(-niche[:, 1]).argpartition(kth=cap - 1)[:cap]]
+        return np.count_nonzero(niche[:, 2])
+
+    def _apply_niching(self, ind: Individual, keyf: str, goal: Goal) -> None:
+        if ind[self._is_niche_winner_key] != 0:
+            ind[keyf] = ind[self._orig_f_key]
+        else:
+            ind[keyf] = np.inf if goal.getDir() == "min" else -np.inf
