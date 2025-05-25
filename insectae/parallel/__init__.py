5f24776ede06bda3bb840ff43e05472e42c96762
Aleksei Nikolashkin <nagvv97@mail.ru>
2025-05-25 18:42:04 +0300

    remove dask executor

--- insectae/parallel/__init__.py
+++ insectae/parallel/__init__.py
@@ -1,5 +1,4 @@
 from .mpi import MPIExecutor
 from .multiprocessing import MultiprocessingExecutor
 from .threading import ThreadingExecutor
-from .dask import DaskExecutor
 from .ray import RayExecutor
